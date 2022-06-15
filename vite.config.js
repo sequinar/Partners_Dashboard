66c3c4d6d894a652b8c1049238c51c9d3e9068de
AlexSlyota <aslyota@sequinar.com>
2022-06-15 13:01:42 +0300

    build fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import EnvironmentPlugin from 'vite-plugin-environment';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: 'esnext',
+  },
   plugins: [vue(),EnvironmentPlugin(['VUE_APP_AUTH0_DOMAIN', 'VUE_APP_AUTH0_CLIENT_KEY', 'VUE_APP_AUTH0_AUDIENCE', 'API_ENDPOINT', 'UEAPP_DOWNLOAD_LINK']),]
 })
