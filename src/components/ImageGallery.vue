b5ed8d890225611a9191050bc9970e575b6be4c3
AlexSlyota <aslyota@sequinar.com>
2022-12-07 14:25:59 +0200

    WLD-585 remove spaces from image name

--- src/components/ImageGallery.vue
+++ src/components/ImageGallery.vue
@@ -1,20 +1,20 @@
 <template>
-    <div ref="imageGallery">
-        <el-upload v-model:file-list="fileList" action="#" list-type="picture-card"
-            :on-preview="handlePictureCardPreview" accept="image/png, image/jpeg"
-            :auto-upload="false" :limit="props.limit" :on-remove="handleRemove">
-            <div class="addButton">
-                <el-icon>
-                    +
-                </el-icon>
-                <span>Add to gallery</span>
-            </div>
-        </el-upload>
+  <div ref="imageGallery">
+    <el-upload v-model:file-list="fileList" action="#" list-type="picture-card"
+      :on-preview="handlePictureCardPreview" accept="image/png, image/jpeg" :auto-upload="false" :limit="props.limit"
+      :on-remove="handleRemove">
+      <div class="addButton">
+        <el-icon>
+          +
+        </el-icon>
+        <span>Add to gallery</span>
+      </div>
+    </el-upload>
 
-        <el-dialog v-model="dialogVisible">
-            <img w-full :src="dialogImageUrl" alt="Preview Image" />
-        </el-dialog>
-    </div>
+    <el-dialog v-model="dialogVisible">
+      <img w-full :src="dialogImageUrl" alt="Preview Image" />
+    </el-dialog>
+  </div>
 </template>
 
 <script setup>
@@ -40,8 +40,10 @@ const fileList = computed({
   get () {
     return props.images
   },
-  set (value) {
-    emits('imageUpdate', value)
+  set (images) {
+    const newImg = images[images.length - 1]
+    newImg.raw = new File([newImg.raw], newImg.raw.name.replace(/\s/g, ''))
+    emits('imageUpdate', images)
   }
 })
 const uploadButton = computed(() => imageGallery.value.querySelector('.el-upload--picture-card'))
@@ -75,45 +77,45 @@ onMounted(() => {
 
 <style lang="scss">
 .el-upload-list--picture-card {
-    gap: 30px;
+  gap: 30px;
 
-    .el-upload-list__item,
-    .el-upload--picture-card {
-        width: 195px;
-        height: 125px;
-        margin: 0;
+  .el-upload-list__item,
+  .el-upload--picture-card {
+    width: 195px;
+    height: 125px;
+    margin: 0;
 
-        .el-upload-list__item-thumbnail {
-            object-fit: cover;
-        }
+    .el-upload-list__item-thumbnail {
+      object-fit: cover;
     }
+  }
 
-    .el-upload--picture-card {
-        order: -1;
-        background-color: rgba(96, 58, 150, 0.05);
-        border: 1px dashed var(--el-color-primary);
+  .el-upload--picture-card {
+    order: -1;
+    background-color: rgba(96, 58, 150, 0.05);
+    border: 1px dashed var(--el-color-primary);
+  }
+
+  .el-upload-list__item-status-label,
+  .el-icon--close-tip {
+    display: none !important;
+  }
+
+  .addButton {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-items: center;
+    color: var(--el-color-primary);
+    font-size: 12px;
+    font-family: 'Montserrat-Bold';
+
+    i {
+      font-style: normal;
+      font-size: 42px;
+      color: var(--el-color-primary);
+      font-family: 'Montserrat';
     }
-
-    .el-upload-list__item-status-label,
-    .el-icon--close-tip {
-        display: none !important;
-    }
-
-    .addButton {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-items: center;
-        color: var(--el-color-primary);
-        font-size: 12px;
-        font-family: 'Montserrat-Bold';
-
-        i {
-            font-style: normal;
-            font-size: 42px;
-            color: var(--el-color-primary);
-            font-family: 'Montserrat';
-        }
-    }
+  }
 }
 </style>
